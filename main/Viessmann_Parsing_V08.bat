44fd138f59c04b364cbeb4a1699bb9bfeb1b3e03
Maxim Grinich <grinikh@gmail.com>
2025-08-29 16:58:57 +0200

    Entferne Pause-Befehle aus den Batch-Dateien und optimiere den Aufruf der Python-Skripte. shop.viessmann-climatesolutions.com URL Änderung

--- main/Viessmann_Parsing_V08.bat
+++ main/Viessmann_Parsing_V08.bat
@@ -3,4 +3,3 @@ cd /d "S:\PyScarper\Viesmann\main"
 call "S:\PyScarper\Viesmann\.venv\Scripts\activate.bat"
 python "S:\PyScarper\Viesmann\main\shop_Viessmann_BD.py"
 python "S:\PyScarper\Viesmann\main\Viessmann_Parsing_V08.py"
-pause
--- main/shop_Viessmann_BD.bat
+++ main/shop_Viessmann_BD.bat
@@ -2,4 +2,3 @@
 cd /d "S:\PyScarper\Viesmann\main"
 call "S:\PyScarper\Viesmann\.venv\Scripts\activate.bat"
 python "S:\PyScarper\Viesmann\main\shop_Viessmann_BD.py"
-pause
